e0756ab9dbb33fc2cded818ba4cd3efc25dde32b
Koyel Basu <koyelbasu240306@gmail.com>
2026-09-15 19:58:16 +0530

    mongodb connected

--- mngdb/server.js
+++ mngdb/server.js
@@ -3,13 +3,19 @@ const mongoose = require("mongoose")
 
 const app = express();
 
-const connectDb = () =>{
-    mongoose.connect(
+const connectDb = async() =>{
+  try{
+    await mongoose.connect(
         "mongodb+srv://koyelbasu2006_db_user:OM2PM9vG2HOLtm7e@cohot-cluster.5gwvuyf.mongodb.net/"
     );
     console.log("mngdb connected");
+}catch (error){
+    console.log("error while connecting db", error);
     
+}
 };
+
+connectDb();
 
 app.get("/", (req, res)=>{
     res.send("Done");
