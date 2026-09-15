f2de090393993abe129a852a686516d6b92b1b62
Koyel Basu <koyelbasu240306@gmail.com>
2026-09-15 22:52:22 +0530

    app & server create

--- mngdb/server.js
+++ mngdb/server.js
@@ -1,27 +1,7 @@
-const express = require("express");
-const mongoose = require("mongoose")
-
-const app = express();
-
-const connectDb = async() =>{
-  try{
-    await mongoose.connect(
-        "mongodb+srv://koyelbasu2006_db_user:OM2PM9vG2HOLtm7e@cohot-cluster.5gwvuyf.mongodb.net/"
-    );
-    console.log("mngdb connected");
-}catch (error){
-    console.log("error while connecting db", error);
-    
-}
-};
-
-connectDb();
-
-app.get("/", (req, res)=>{
-    res.send("Done");
-})
+const app = require("./src/app")
 
 app.listen(600, () => {
     console.log("Server is running on port 600");
-    
 });
+
+
